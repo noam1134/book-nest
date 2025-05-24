0fa26cc547bf9435fe4be48f359d1afd1ae8b8cf
noam1134 <90392508+noam1134@users.noreply.github.com>
2025-05-24 20:58:06 +0300

    implemented supabase logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
 	import { Header } from '$components';
 	import './../app.css';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-	let { children } = $props();
+	let { children, data } = $props();
+	let { session, supabase, user } = $derived(data);
+
+	$effect(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
+
+	$inspect(session);
 </script>
 
 <Header></Header>
